50044f6d052b47b57e099268f6cfa9653776cf10
mohamed-mahmoud-hanenne <medhanenne38@gmail.com>
2025-03-23 14:41:39 +0000

    cmt

--- src/app/prediction/prediction.component.ts
+++ src/app/prediction/prediction.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-prediction',
@@ -28,14 +29,33 @@ export class PredictionComponent {
   constructor(private http: HttpClient) {}
 
   onSubmit() {
-    this.http.post('https://projet-stocha.onrender.com/predict', this.formData).subscribe(
-      (response) => {
-        this.result = response;
-      },
-      (error) => {
-        console.error('Erreur lors de la requête API', error);
-      }
-    );
+    this.http.post<{ decision: string }>('https://projet-stocha.onrender.com/predict', this.formData)
+      .subscribe(
+        (response) => {
+          if (response.decision === "acceptée") {
+            Swal.fire({
+              icon: 'success',
+              title: 'Succès',
+              text: 'La demande a été acceptée !'
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Refusé',
+              text: 'Désolé, la demande a été refusée.'
+            });
+          }
+        },
+        (error) => {
+          console.error('Erreur lors de la requête API', error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Erreur',
+            text: 'Une erreur est survenue lors de la requête.'
+          });
+        }
+      );
   }
+  
 
 }
